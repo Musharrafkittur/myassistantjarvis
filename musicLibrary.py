2e7e9157bf3505322b61507d724a3764a830461e
Musharrafkittur <141600517+Musharrafkittur@users.noreply.github.com>
2024-08-09 11:42:50 +0530

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,4 @@
+music   ={ "softly": "https://www.youtube.com/watch?v=cWMxCE2HTag",
+          "company":"https://www.youtube.com/watch?v=GuGYYdwcPYw",
+          "grind":"https://www.youtube.com/watch?v=O4YEtMj1B50"
+          }
